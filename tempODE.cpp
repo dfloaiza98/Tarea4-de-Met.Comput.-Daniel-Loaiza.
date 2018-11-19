e178ba39550bd5bfeec87f41564280decc7f9031
dfloaiza98 <42219640+dfloaiza98@users.noreply.github.com>
2018-11-18 19:50:42 -0500

    Add files via upload
    Primer avance del punto2, ODE.

---
+++ tempODE.cpp
@@ -0,0 +1,22 @@
+//Punto2 de la tarea4 de metodos computacionales  2018-2. Daniel Francisco Loaiza Saavedra, cod= 201615258.
+
+//Importacion de librerias.
+#include <cstdlib>
+#include <math.h>
+#include <iostream>
+
+//Constantes a ser utilizadas durante la solucion.
+double g=10,m=0.2,c=0.2, v=300,h=0.01;
+
+using namespace std;
+
+//Metodo que calcula la norma de un vector dadas dos componentes.
+double norm(double ux,double uy){
+    return (sqrt(ux*ux+uy*uy));
+}
+
+int main(){
+
+
+return 0;
+}
