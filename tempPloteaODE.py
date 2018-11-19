035c2b33d59529263ca21114dc44fb6a041b514e
dfloaiza98 <42219640+dfloaiza98@users.noreply.github.com>
2018-11-19 18:07:11 -0500

    Add files via upload
    Ya estan los plots correspondientes a las partes 1 y 2 del punto2 de esta tarea.

--- tempPloteaODE.py
+++ tempPloteaODE.py
@@ -44,3 +44,54 @@ datosDy60= []
 datosDx70= []
 datosDy70= []
 
+k=1
+l= contadores[0]+2
+for j in range(k,l):
+	datosDx10.append(float(datosangulos[j][3]))
+	datosDy10.append(float(datosangulos[j][4]))
+k=1 +l
+l= contadores[1]+contadores[0]+4
+for j in range(k,l):
+	datosDx20.append(float(datosangulos[j][3]))
+	datosDy20.append(float(datosangulos[j][4]))
+k=1 + l
+l= contadores[0]+ contadores[1] + contadores[2]+6
+for j in range(k,l):
+	datosDx30.append(float(datosangulos[j][3]))
+	datosDy30.append(float(datosangulos[j][4]))
+k=1 + l
+l= contadores[0]+ contadores[1] + contadores[2]+  contadores[3]+8
+for j in range(k,l):
+	datosDx40.append(float(datosangulos[j][3]))
+	datosDy40.append(float(datosangulos[j][4]))
+k=1 + l
+l= contadores[0]+ contadores[1] + contadores[2]+ contadores[3]+contadores[4]+ 10
+for j in range(k,l):
+	datosDx50.append(float(datosangulos[j][3]))
+	datosDy50.append(float(datosangulos[j][4]))
+k=1 + l
+l= contadores[0]+ contadores[1] + contadores[2]+ contadores[3]+contadores[4]+ contadores[5]+12
+for j in range(k,l):
+	datosDx60.append(float(datosangulos[j][3]))
+	datosDy60.append(float(datosangulos[j][4]))
+k=1+l
+l= contadores[0]+ contadores[1] + contadores[2]+ contadores[3]+contadores[4]+ contadores[5]+contadores[6]+14
+for j in range(k,l):
+	datosDx70.append(float(datosangulos[j][3]))
+	datosDy70.append(float(datosangulos[j][4]))
+
+
+plt.figure()
+plt.title("Movimiento del proyectil para distintos angulos")
+plt.plot(datosDx10,datosDy10,label="10")
+plt.plot(datosDx20,datosDy20,label="20")
+plt.plot(datosDx30,datosDy30,label="30")
+plt.plot(datosDx40,datosDy40,label="40")
+plt.plot(datosDx50,datosDy50,label="50")
+plt.plot(datosDx60,datosDy60,label="60")
+plt.plot(datosDx70,datosDy70,label="70")
+plt.ylabel("Posicion en y[m]")
+plt.xlabel("Posicion en x[m]")
+plt.legend(loc="best")
+plt.savefig("Desplazamientoproyectilvariosangulos.pdf")
+
