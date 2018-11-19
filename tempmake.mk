702cfd89a1e87f4cbb1c1af3196b2c09b42badd8
dfloaiza98 <42219640+dfloaiza98@users.noreply.github.com>
2018-11-19 17:12:24 -0500

    Add files via upload
    Ya el make se actualiza para los tres archivos txt que generan ODE.cpp y PDE.cpp

--- tempmake.mk
+++ tempmake.mk
@@ -1,7 +1,11 @@
-trayectoria.dat: ODE.cpp
+datostrayectoria.txt: ODE.cpp
 	g++ ODE.cpp
 	./a.out
 
-datosPDE.dat: PDE.cpp
+datosangulo.txt: ODE.cpp
+	g++ ODE.cpp
+	./a.out
+
+datosPDE.txt: PDE.cpp
 	g++ PDE.cpp
 	./a.out
