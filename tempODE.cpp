2f9a741cb1832b5373210ccca40886054cd14615
dfloaiza98 <42219640+dfloaiza98@users.noreply.github.com>
2018-11-19 07:40:36 -0500

    Add files via upload
    Ahora ya hay una funcion para definir la ODE que se tiene que resolver.

--- tempODE.cpp
+++ tempODE.cpp
@@ -10,11 +10,18 @@ double g=10,m=0.2,c=0.2, v=300,h=0.01;
 
 using namespace std;
 
-//Metodo que calcula la norma de un vector dadas dos componentes.
+//Metodo que calcula la norma de un vector dadas dos componentes que son la coordenada de x y y.
 double norm(double ux,double uy){
     return (sqrt(ux*ux+uy*uy));
 }
 
+
+//Funcion para definir la ecuacion diferencial ordinaria. 
+double f(double t, double ux,double uy, bool xx){
+    double x=-c*norm(ux,uy)*ux/m, y=-g-c*norm(ux,uy)/m*uy;
+    if(xx)return (x);
+    return (y);
+}
 int main(){
 
 
