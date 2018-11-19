ea5d59e744759539bdd286c0437de32235323575
dfloaiza98 <42219640+dfloaiza98@users.noreply.github.com>
2018-11-19 08:27:21 -0500

    Add files via upload
    Ya hay una funcion que calcula las iteraciones (k1,k2,k3 y k4)y que debe funcionar

--- tempODE.cpp
+++ tempODE.cpp
@@ -22,6 +22,20 @@ double f(double t, double ux,double uy, bool xx){
     if(xx)return (x);
     return (y);
 }
+
+//Funcion que itera a la cual le entra por parametro el tiempo, velocidad en x , velocidad en y un booleano para saber si la funcion calculara la funcion anterior f en la coordenada x o en coordenada y.
+double iter(double t, double ux,double uy,bool xx){
+    double kx=f(t,ux,uy,true);
+    double ky=f(t,ux,uy,false);
+    double k2x= f(t+h/2,ux+1/2*h*kx,uy+1/2*h*ky ,true);
+    double k2y= f(t+h/2,ux+1/2*h*kx,uy+1/2*h*ky ,false);
+    double k3x= f(t+h/2,ux+1/2*h*k2x,uy+1/2*h*k2y ,true);
+    double k3y= f(t+h/2,ux+1/2*h*k2x,uy+1/2*h*k2y ,false);
+    double k4x= f(t+h/2,ux+1/2*h*k3x,uy+1/2*h*k3y ,true);
+    double k4y= f(t+h/2,ux+1/2*h*k3x,uy+1/2*h*k3y ,false);
+    if(xx) return (ux+h/6*(kx+2*k2x+2*k3x+k4x));
+    return (uy+h/6*(ky+2*k2y+2*k3y+k4y));
+}
 int main(){
 
 
